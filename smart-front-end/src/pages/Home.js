e4d3c474491aba977f310904491e6cd28bd3edd9
Aliaa0o0 <aliaalolamohamed3@gmail.com>
2026-01-29 00:43:29 +0200

    Update Home.js

--- smart-front-end/src/pages/Home.js
+++ smart-front-end/src/pages/Home.js
@@ -1,4 +1,6 @@
 import React, { useEffect, useRef } from "react";
+import { Link } from "react-router-dom";
+import Navbar from "./Navbar.js";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 // Images
@@ -699,4 +701,5 @@ export default function App() {
       </div>
     </>
   );
+
 }
